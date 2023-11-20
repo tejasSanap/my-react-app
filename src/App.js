a0b580cccb0c9445c2e8cc598e33be866eba8ff9
Tejas Sanap <sanaptejas98@gmail.com>
2023-11-21 02:37:03 +0530

    tejas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data,setData] = useState(null);
+  const fetchData = async()=>{
+      const res = await axios.get('http://localhost:8900/pets',{ rejectUnauthorized: false });
+      console.log(res.data)
+      setData(res.data)
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
+  console.log("count")
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {data}
         </p>
         <a
           className="App-link"
@@ -15,7 +27,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
